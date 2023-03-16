33cf251e6adca147cda0f2dc9a5a20598984c1c2
Glaolle <glaolle@gmail.com>
2023-03-16 09:42:16 +0300

    Change version easy-rsa

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,14 +5,14 @@ mkdir -p {easyrsa,ccd}
 cd easyrsa
 
 if [ ! -f easyrsa ]; then
-  curl -sL https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz | tar -xzv --strip-components=1 -C .
+  curl -sL https://github.com/OpenVPN/easy-rsa/releases/download/v3.1.2/EasyRSA-3.1.2.tgz | tar -xzv --strip-components=1 -C .
 fi
 
 if [ -d pki ]; then
   exit 0
 fi
 
-./easyrsa init-pki
-echo "ca" | ./easyrsa build-ca nopass
-./easyrsa build-server-full server nopass
-./easyrsa build-client-full client nopass
+./easyrsa --batch init-pki
+./easyrsa --batch build-ca nopass
+./easyrsa --batch build-server-full server nopass
+./easyrsa --batch build-client-full client nopass
